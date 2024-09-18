26a3cb241d7874c7e006f456147fbb766b1f5f76
johnw <johnw@Amon-Ra>
2024-09-17 22:28:29 -0400

    assignment 2 update

--- LSP_FALL_2024/src/Org/howard/edu/lsp/assignment2/assignment2.java
+++ LSP_FALL_2024/src/Org/howard/edu/lsp/assignment2/assignment2.java
@@ -1,4 +1,5 @@
 package Org.howard.edu.lsp.assignment2;
+
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
@@ -8,25 +9,18 @@ import java.util.Map;
 public class assignment2 {
 
     public static void main(String[] args) {
-        // Ensure a filename is provided
-        if (args.length != 1) {
-            System.out.println("Usage: java WordCount <filename>");
-            return;
-        }
+        String filename = "src/Org/howard/edu/lsp/assignment2/sample.txt";;
 
-        String filename = args[0];
         HashMap<String, Integer> wordCountMap = new HashMap<>();
 
-        // Reading the file
+        
         try (BufferedReader br = new BufferedReader(new FileReader(filename))) {
             String line;
 
-            // Process each line
+            
             while ((line = br.readLine()) != null) {
-                // Convert the line to lower case and split it into words
                 String[] words = line.toLowerCase().split("\\W+");
 
-                // Count each word
                 for (String word : words) {
                     if (!word.isEmpty()) {
                         wordCountMap.put(word, wordCountMap.getOrDefault(word, 0) + 1);
